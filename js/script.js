59c283a43e5903e416d19804b872b02144df34e1
Russell Skaggs <contact@russellskaggs.com>
2013-11-20 17:40:47 -0500

    Sassified and made some updates to CSS and javascript

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var $rs = {};
+(function () {
+	var body = document.getElementsByTagName('body')[0],
+		navList = document.getElementsByClassName('nav-list')[0];
+	function toggleClass($element, $class) {
+		var classList = $element.className.split(' '),
+			classIndex = classList.indexOf($class);
+		
+		if (classIndex > -1) {
+			classList.splice(classIndex, 1);
+		} else {
+			classList.push($class);
+		}
+		$element.className = classList.join(' ');	
+	}
+	toggleClass(navList, '-no-animate');
+	toggleClass(document.getElementsByTagName('body')[0], 'js-enabled');
+	setTimeout(function () {
+		toggleClass(navList, '-no-animate');
+	}, 100);
+	
+	$rs.toggleNavigation = function () {
+		toggleClass(navList, '-open');
+	}
+	
+	document.getElementsByClassName('nav-toggle')[0].addEventListener("click", $rs.toggleNavigation, false);
+	document.getElementsByClassName('nav-toggle')[0].addEventListener("touchstart", $rs.toggleNavigation, false);
+	
+})();
